e2530a2fd205d344d7c35aae09c53991b8a1e46c
fajarachmadyusup13 <fajarachmady13@gmail.com>
2018-01-09 13:35:58 +0700

    add PostNew component

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import _ from 'lodash';
 
 import { fetchData } from "../actions";
 
 
 class PostIndex extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.fetchData();
     }
 
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        });
+    }
 
     render() {
         return (
             <div>
-               Post Index 
+               <h3>Posts</h3> 
+               <ul className="list-group">
+                    {this.renderPosts()}
+               </ul>
             </div>
         );
     }
 }
 
-export default connect(null, {fetchData})(PostIndex);
+function mapStateToProps(state) {
+    return {
+        posts: state.posts
+    };
+}
+
+export default connect(mapStateToProps, {fetchData})(PostIndex);
